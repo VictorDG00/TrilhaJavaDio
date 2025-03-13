26424a47bc408472d22a3a913eb6a1a22d34d8a1
Victordg00 <victord.goncalves@outlook.com>
2025-03-13 16:32:55 -0300

    fim da aula 3

---
+++ JavaBasico/TiposVariaveis/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    byte umByte = 0; // tem 1 byte de memoria  (-128 ate 127)
+    short doisByte = 1; // tem 2 byte de memoria (-32.768 ate 32.767)
+    int numeral = 1; // tem 4 bytes de memoria, faz as conta
+    long naosei2 = 3232324254353L; // tem 8 bytes de memoria, //, precisa ser encerrado com L
+    float pontoFlutuante = 3.14F; // tambem tem 4 byte de memoria, sem F no final ele acha que e double
+    double espacoExtra = 2323; // esse malandro tem 8, faz as conta
+    boolean verdadeiroFalso = false; // binario, false ou true
+    char naosei3 = 323;
+    String texto = "texto mesmo";
+}
